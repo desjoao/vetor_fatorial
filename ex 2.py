90176c94e182b20f54e3baad5ce49af5b697cd0f
desjoao <98698809+desjoao@users.noreply.github.com>
2023-11-17 23:02:19 -0300

    Update ex 2.py
    Correção da primeira condicional de verificação de valores de entrada, admitindo o valor zero como entrada.

--- ex 2.py
+++ ex 2.py
@@ -2,22 +2,24 @@ def leitura():
     vetor = [0]*10
     for i in range(10):
         vetor[i] = int(input(f'Informe um {i + 1}º valor inteiro e positivo: '))
-        while vetor[i] <=0:
+        while vetor[i] <0:
             vetor[i] = int(input(f'Informe um {i + 1}º valor inteiro e POSITIVO: '))
     return vetor
 
 def fatorial(vetor):
     vetor2 = [0]*10
     for i in range(10):
-        vetor2[i] = vetor[i]
-        x = vetor[i] - 1
-        while x > 0:
-            vetor2[i]*=x
-            x-=1
+        if vetor[i] == 0:
+            vetor2[i] = 1
+        else:
+            vetor2[i] = vetor[i]
+            x = vetor[i] - 1
+            while x > 0:
+                vetor2[i]*=x
+                x-=1
     return vetor2
 
 valores = leitura()
 fatoriais = fatorial(valores)
 
-print(valores)
-print(fatoriais)
+print(f'Para os números {valores}, seus fatoriais equivalentes são {fatoriais}.')
